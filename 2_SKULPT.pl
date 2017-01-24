9694d6a51af0b0e5ec8f04d6ce4cf0a2774cb176
Prince of Archives <rich.tijerina@gmail.com>
2017-01-24 14:09:36 -0700

    Create 2_SKULPT.pl

---
+++ 2_SKULPT.pl
@@ -0,0 +1,21 @@
+use strict; use warnings;
+open(my $fh, "<", 'tmp.txt');
+my @line = readline $fh; chomp @line; close $fh;
+my @name; my @date_of_service; my @payment;
+my $count = 0; my $i = 0; my $n = 0;
+while($i < $#line) {
+    if (($line[$i] =~ /^[A-Z]/) and ($line[$i] =~ /,/)) {
+        $name[$count] = $line[$i]; $i++;
+        $date_of_service[$count] = $line[$i];
+        $i++; $i++; $i++;
+        $payment[$count] = $line[$i];
+        $i++;
+        $count++;
+    }
+    else
+        { $i++; }
+}
+while ($n < $count) {
+    print "$name[$n]  :  $date_of_service[$n]  :  $payment[$n]\n";
+    $n++;
+}
