491a63e756c3bdc81a3d7e066ddf786539aca5d6
Prince of Archives <rich.tijerina@gmail.com>
2017-01-24 14:05:03 -0700

    Update 1_RIPP.pl

--- 1_RIPP.pl
+++ 1_RIPP.pl
@@ -2,18 +2,18 @@ use warnings; use strict;
 use CAM::PDF;
 #########################
 # RIPP - scrape pdf files
-my $file = 'tst.pdf';
+my ($file) = @ARGV;
 my $doc; my $str; my $batch; my $end; my $p;
 $doc = CAM::PDF->new($file) || die "$CAM::PDF::errstr\n";
 open(my $fh, '>', 'tmp.txt'); # must verify data struct post-extraction 
 my $pages = $doc->numPages(); $p = '1'; $end = $pages; $end++;
 while ($p != $end)
-{
+{ # remove unnessary data on rip
       $str = $doc->getPageText($p);
-      $str =~ s/TRASHDATA_item//g;
+      $str =~ s/TRASHDATA_item//g; # remove individual strings
       my $rm1 = "Page $p of ".$pages;
-      my $rm2 = "TRASHDATA_begin"; my $rm22 = "TRASHDATA_end";
-      $str =~ s/$rm1.*//s;
+      my $rm2 = "TRASHDATA_begin"; my $rm22 = "TRASHDATA_end"; 
+      $str =~ s/$rm1.*//s; # isolate & remove entire string sections
       $str =~ s/$rm2.*$rm22//s;
       print $fh $str;
       $p++;
